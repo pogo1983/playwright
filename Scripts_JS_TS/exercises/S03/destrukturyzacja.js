e0371566c1a51000aee31f8fcd3c8758330dd1ae
Marcin Pogorzelski <marcinpogorzelski@MacBook-Air-Marcin.local>
2026-01-23 15:44:22 +0100

    destrukturyzacja

--- Scripts_JS_TS/exercises/S03/destrukturyzacja.js
+++ Scripts_JS_TS/exercises/S03/destrukturyzacja.js
@@ -81,3 +81,101 @@ person.nationality = "Polska";
 console.log(person)
 person.age=43// for person = {age:43} let must be used not const
 console.log(person)
+
+//
+const employee = {
+  id: 1,
+  name: "Jan",
+  position: { title: "Developer", department: "IT" },
+};
+
+const {
+  position: { title, department },
+} = employee;
+
+console.log(title);    // "Developer"
+console.log(department); // "IT"
+
+//Przykład 3: Destrukturyzacja w parametrach funkcji
+// ❌ Tradycyjnie - musisz pamiętać kolejność!
+function createUser(name, age, email, city, country, phone) {
+  // ...
+}
+createUser("Jan", 30, "jan@example.com", "Warsaw", "Poland", "123456");
+// Jaka była kolejność? Który parametr to co?
+
+// ✅ Z destrukturyzacją - nie ważna kolejność!
+function createUserDestr({ name, age, email, city, country, phone }) {
+  console.log(name, age);
+}
+createUser({ 
+  email: "jan@example.com",
+  name: "Jan",  // kolejność nie ma znaczenia!
+  age: 30,
+  phone: "123456",
+  city: "Warsaw",
+  country: "Poland"
+});
+
+
+
+// Wyjaśnienie:
+// BEZ destrukturyzacji
+function createUser2(options) {
+  const result = options.name + options.age;  // options.name, options.age
+  return result;
+}
+
+// Z destrukturyzacją
+function createUser3({ name, age }) {
+  const result = name + age;  // bezpośrednio name, age
+  return result;
+}
+
+// Wywołanie jest identyczne dla obu!
+createUser2({ name: "Jan", age: 30 });
+createUser3({ name: "Jan", age: 30 });
+console.log(createUser2({ name: "Jan", age: 30 }));
+console.log(createUser3({ name: "Jan", age: 30 })); 
+
+const wynik = createUser2({ name: "Jan", age: 30 })
+console.log(wynik);
+const wynik2 = createUser3({ name: "Jan", age: 30 })
+console.log(wynik2);
+//console.log(`Wynik to: ${name} i ${age} lat`);
+
+//z ladnym formatowaniem wyniku i wieksza ilsocia zwracanych danych
+function createUser4({ name, age }) {
+  const result = name + age;  // bezpośrednio name, age
+  return {result, name, age};
+}
+const wynik4 = createUser4({ name: "Jan", age: 30 })
+console.log(`Wynik to: ${wynik4.name} \
+i ${wynik4.age} lat i ${wynik4.result} jako suma!`);
+
+
+//uzycie spread operatora do przypisania pozostalych wartosci obiektu
+const person2 = {
+  firstName: "Anna",
+  lastName: "Kowalska",
+  age: 30,
+};
+const { firstName, ...otherDetails } = person2;
+
+console.log(firstName);  // "Anna"
+console.log(otherDetails); // { lastName: "Kowalska", age: 30 }
+
+//destrukturyzacja tablic
+const colors2 = ["czerwony", "zielony", "niebieski"];
+
+const [firstColor2, secondColor2, thirdColor3] = colors2;
+
+console.log(firstColor2);  // "czerwony"
+console.log(secondColor2); // "zielony"
+console.log(thirdColor3);  // "niebieski"
+
+
+//pomijanie elementow
+const [, , thirdColor4] = colors2;
+
+console.log(thirdColor4); // "niebieski"
